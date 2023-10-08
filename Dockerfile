9736f27e21b5fcadd614d1aa22ef511968a202e0
Arul Agrawal <me@arul.io>
2023-10-08 16:45:01 +0000

    Bun

--- Dockerfile
+++ Dockerfile
@@ -1,27 +1,21 @@
 # Use an official Node runtime as the base image
-FROM node:18-alpine as builder
+FROM docker.io/oven/bun:1-alpine as builder
 
 # Set the working directory in the docker image
 WORKDIR /usr/src/app
 
 # Copy package.json and yarn.lock into the working directory
-COPY package.json yarn.lock ./
+COPY package.json bun.lockb index.ts ./
 
 # Install the application dependencies using 'yarn install'
-RUN yarn install --frozen-lockfile --production
+RUN bun install --production --no-cache
 
-# Copy the rest of the application code into the working directory
-COPY . .
+# CMD bun index.js
+FROM docker.io/oven/bun:1-distroless as final
 
-# Second stage - distroless Node.js image
-FROM gcr.io/distroless/nodejs:18
-
-# Set the working directory again
 WORKDIR /usr/src/app
 
 # Copy over dependencies and built resources from first stage
 COPY --from=builder /usr/src/app .
 
-# Define the command that should be executed
-# when the docker image is run.
-CMD ["index.js"]
+CMD ["index.ts"]
---
+++ index.ts
@@ -0,0 +1,131 @@
+import { Router } from '@stricjs/router';
+import { query } from '@stricjs/utils';
+
+const whatsapp_token = Bun.env.WHATSAPP_TOKEN;
+const openai_token = Bun.env.OPENAI_API_KEY;
+const verify_token = Bun.env.VERIFY_TOKEN;
+
+const app = new Router({ hostname: "0.0.0.0", port: Bun.env.PORT || 1337 });
+
+const getAudioUrl = async (audio_id) => {
+	const request = await fetch(`https://graph.facebook.com/v13.0/${audio_id}`, {
+		method: "GET",
+		headers: { Authorization: `Bearer ${whatsapp_token}` },
+	});
+	const response = await request.json();
+	return response.url;
+};
+
+const downloadAudio = async (audio_url) => {
+	const request = await fetch(audio_url, {
+		method: "GET",
+		headers: { Authorization: `Bearer ${whatsapp_token}` },
+	});
+	const response = await request.arrayBuffer();
+	return new Blob([response], { type: "audio/ogg" });
+};
+
+const transcribeAudio = async (audio_buffer) => {
+	const form = new FormData();
+
+	form.append("file", audio_buffer, "audio.ogg");
+	form.append("model", "whisper-1");
+
+	const request = await fetch(
+		"https://api.openai.com/v1/audio/transcriptions",
+		{
+			method: "POST",
+			body: form,
+			headers: {
+				Authorization: `Bearer ${openai_token}`,
+			},
+		},
+	);
+
+	const response = await request.json();
+	return response.text;
+};
+
+const sendMessage = async (phone_number_id, from, text) => {
+	const request = await fetch(
+		`https://graph.facebook.com/v12.0/${phone_number_id}/messages`,
+		{
+			method: "POST",
+			body: JSON.stringify({
+				messaging_product: "whatsapp",
+				to: from,
+				text: { body: text },
+			}),
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${whatsapp_token}`,
+			},
+		},
+	);
+	const response = await request.json();
+	return response.messages[0].id;
+};
+
+const markAsRead = async (phone_number_id, message_id) => {
+	const request = await fetch(
+		`https://graph.facebook.com/v18.0/${phone_number_id}/messages`,
+		{
+			method: "POST",
+			body: JSON.stringify({
+				messaging_product: "whatsapp",
+				status: "read",
+				message_id: message_id,
+			}),
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${whatsapp_token}`,
+			},
+		},
+	);
+};
+
+app.post("/webhook", async (ctx) => {
+	try {
+		if (!ctx.data.object) return new Response("Invalid Request", { status: 400 });
+
+		const changes = ctx.data.entry?.[0]?.changes;
+		const value = changes?.[0]?.value;
+		const messages = value?.messages?.[0];
+		if (messages) {
+			const metadata = value.metadata;
+			const phone_number_id = metadata?.phone_number_id;
+			const from = messages.from;
+			const audio_id = messages?.audio?.id;
+
+			if (phone_number_id && from && audio_id) {
+				await markAsRead(phone_number_id, messages.id);
+
+				const audio_url = await getAudioUrl(audio_id);
+				const audio = await downloadAudio(audio_url);
+
+				const transcription = await transcribeAudio(audio);
+				await sendMessage(phone_number_id, from, transcription);
+			}
+		}
+		return new Response("EVENT_RECEIVED", { status: 200 });
+	} catch (error) {
+		console.error("An error occurred:", error);
+		return new Response("An error occurred", { status: 500 });
+	}
+}, { body: 'json' });
+
+app.get("/webhook", (ctx) => {
+	const parsed = query(ctx.url.substring(ctx.query + 1));
+	const mode = parsed["hub.mode"];
+	const token = parsed["hub.verify_token"];
+	const challenge = parsed["hub.challenge"];
+
+	if (mode && token && mode === "subscribe" && token === verify_token) {
+		console.log("WEBHOOK_VERIFIED");
+		return new Response(challenge, { status: 200 });
+	} else {
+		return new Response("Invalid Request", { status: 403 });
+	}
+});
+
+app.listen();
